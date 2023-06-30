997c7ebe1913222ca6b6ece7f1d28af64bfa9bff
James McArthur <77163783+jamezmca@users.noreply.github.com>
2023-06-30 21:31:38 +1200

    cool

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -84,6 +84,7 @@
         },
     ];
 </script>
+
 <header
     bind:clientHeight={headerHeight}
     class={"duration-200 sticky top-0 relative border-b border-solid  " +
@@ -347,28 +348,38 @@
         <!-- <i class="fa-solid fa-envelopes-bulk mx-auto text-2xl" /> -->
 
         <form
+            name="contact"
+            method="POST"
             class="flex flex-col gap-4 max-w-[500px] w-full mx-auto text-slate-800 text-xs sm:text-sm"
+            data-netlify="true"
         >
             <input
+                name="name"
                 type="text"
                 placeholder="Full Name"
                 class="outline-none focus:outline none bg-white p-2 rounded border-2 border-solid border-transparent hover:border-indigo-200 focus:border-indigo-200 duration-200"
             />
             <input
+                required
+                name="email"
                 type="email"
                 placeholder="Email Address"
                 class="outline-none focus:outline none bg-white p-2 rounded border-2 border-solid border-transparent hover:border-indigo-200 focus:border-indigo-200 duration-200"
             />
             <input
+                name="subject"
                 type="text"
                 placeholder="Subject"
                 class="outline-none focus:outline none bg-white p-2 rounded border-2 border-solid border-transparent hover:border-indigo-200 focus:border-indigo-200 duration-200"
             />
             <textarea
+                required
+                name="message"
                 placeholder="Your message"
                 class="outline-none focus:outline none bg-white p-2 rounded border-2 border-solid border-transparent hover:border-indigo-200 focus:border-indigo-200 duration-200 h-32 resize-none"
             />
             <button
+                type="submit"
                 class="border-white border-solid border-[1.5px] text-white px-4 py-2 rounded duration-200 hover:border-indigo-200"
                 >Submit</button
             >
